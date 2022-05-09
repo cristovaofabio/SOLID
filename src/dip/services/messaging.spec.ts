f9628941da3f317205c8ed640ec81dc098dce735
Cristovao <cristovaofabio3@gmail.com>
2022-05-09 08:53:03 -0300

    using spyOn

--- src/dip/services/messaging.spec.ts
+++ src/dip/services/messaging.spec.ts
@@ -1,6 +1,30 @@
-describe('TESTING SOMETHING', () => {
-    it('test description', () => {
-        const number = 1;
-        expect(number).toBe(1);
+import { Messaging } from './messaging';
+
+describe('MESSAGING', () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it('should return undefined', () => {
+        const sut = new Messaging();
+        const message = 'Hello!';
+
+        expect(sut.sendMessage(message)).toBeUndefined;
+    });
+
+    it('should call console.log once', () => {
+        const sut = new Messaging();
+        const message = 'Hello!';
+        const consoleSpy = jest.spyOn(console, 'log');
+
+        expect(sut.sendMessage(message)).toBeUndefined;
+        expect(consoleSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call console.log once "Hello!"', () => {
+        const sut = new Messaging();
+        const message = 'Hello!';
+        const consoleSpy = jest.spyOn(console, 'log');
+
+        expect(sut.sendMessage(message)).toBeUndefined;
+        expect(consoleSpy).toHaveBeenCalledWith('Hello!');
     });
 });
